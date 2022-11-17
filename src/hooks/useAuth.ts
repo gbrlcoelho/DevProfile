ef78d7c4f2864a278a5de524a125aa7bf2d1ed15
gccano <gccano@viptech.com.br>
2022-11-17 12:45:00 -0300

    feat(hooks): createad use auth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,13 @@
 import {useContext} from 'react'
 import {AuthContext} from '../context/Auth/Auth'
+import {AuthContextData} from '../context/Auth/types'
 
-export const useAuth = () => {
+export const useAuth = (): AuthContextData => {
   const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+
   return context
 }
