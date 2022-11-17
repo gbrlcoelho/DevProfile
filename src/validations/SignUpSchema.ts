999ae501b299e9a3587c08e0e489de23a95659ab
gccano <gccano@viptech.com.br>
2022-11-16 21:53:41 -0300

    refactor(validations): refactored sign up validation schema

--- src/validations/SignUpSchema.ts
+++ src/validations/SignUpSchema.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 
 const signUpSchema = Yup.object().shape({
-  fullName: Yup.string().required('Informe seu nome completo.'),
+  name: Yup.string().required('Informe seu nome completo.'),
   email: Yup.string().email('Email inválido.').required('Informe o email.'),
   password: Yup.string().required('Informe a senha.'),
 })
