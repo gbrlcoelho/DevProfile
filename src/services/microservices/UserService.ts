d94fcc67b3968b1d87ecc7c6ee8f2e4e727f0041
gccano <gccano@viptech.com.br>
2022-11-18 12:01:47 -0300

    feat(services): forgot password service

--- src/services/microservices/UserService.ts
+++ src/services/microservices/UserService.ts
@@ -3,13 +3,19 @@ import {ApiResponse, IPostUser, IUserResponse} from './types'
 
 class UserService {
   private postUser: string
+  private forgotPassword: string
 
   constructor() {
     this.postUser = '/users'
+    this.forgotPassword = '/password/forgot'
   }
 
   postNewUser = async (data: IPostUser): ApiResponse<IUserResponse> => {
     return await axios.post(`${this.postUser}`, data)
+  }
+
+  postForgotPassword = async ({email}: Partial<IPostUser>): ApiResponse<IUserResponse> => {
+    return await axios.post(`${this.forgotPassword}`, email)
   }
 }
 export default new UserService()
