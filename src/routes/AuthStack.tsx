bf6fe31a5a559d87e08e4027bea75a4c2a8ba4be
gccano <gccano@viptech.com.br>
2022-11-18 12:01:14 -0300

    refactor(routes): updated auth stack routes

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -1,5 +1,6 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import React from 'react'
+import {ForgotPassword} from '../views/ForgotPassword/ForgotPassword'
 import {SignIn} from '../views/SignIn/SignIn'
 import {SignUp} from '../views/SignUp/SignUp'
 
@@ -10,6 +11,7 @@ export const AuthStack = () => {
     <Navigator screenOptions={{headerShown: false}}>
       <Screen name='SignIn' component={SignIn} />
       <Screen name='SignUp' component={SignUp} />
+      <Screen name='ForgotPassword' component={ForgotPassword} />
     </Navigator>
   )
 }
